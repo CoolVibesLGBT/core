652af118dd2d3ae4a5fe8e2569e0ae71e486a8e2
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2026-01-16 09:47:10 +0300

    fixes

--- models/domain.go
+++ models/domain.go
@@ -5,13 +5,19 @@ import "strings"
 type DomainName string
 
 const (
-	CoolVibesLGBT DomainName = "coolvibes.lgbt"
-	CoolVibesAPP  DomainName = "coolvibes.app"
-	CoolVibesIO   DomainName = "coolvibes.io"
+	CoolVibesLGBT    DomainName = "coolvibes.lgbt"
+	CoolVibesAPP     DomainName = "coolvibes.app"
+	CoolVibesIO      DomainName = "coolvibes.io"
+	CoolVibesLGBTAPI DomainName = "api.coolvibes.lgbt"
+	CoolVibesAPPAPI  DomainName = "api.coolvibes.app"
+	CoolVibesIOAPI   DomainName = "api.coolvibes.io"
 
-	KEWLSWAPCOM DomainName = "kewlswap.com"
-	KEWLSWAPIO  DomainName = "kewlswap.io"
-	KEWLSWAPAPP DomainName = "kewlswap.app"
+	KEWLSWAPCOM    DomainName = "kewlswap.com"
+	KEWLSWAPIO     DomainName = "kewlswap.io"
+	KEWLSWAPAPP    DomainName = "kewlswap.app"
+	APIKEWLSWAPCOM DomainName = "api.kewlswap.com"
+	APIKEWLSWAPIO  DomainName = "api.kewlswap.io"
+	APIKEWLSWAPAPP DomainName = "api.kewlswap.app"
 )
 
 type DomainKind string
@@ -23,14 +29,20 @@ const (
 
 var DomainMap = map[DomainName]DomainKind{
 	// CoolVibes
-	CoolVibesLGBT: CoolVibes,
-	CoolVibesAPP:  CoolVibes,
-	CoolVibesIO:   CoolVibes,
+	CoolVibesLGBT:    CoolVibes,
+	CoolVibesAPP:     CoolVibes,
+	CoolVibesIO:      CoolVibes,
+	CoolVibesLGBTAPI: CoolVibes,
+	CoolVibesAPPAPI:  CoolVibes,
+	CoolVibesIOAPI:   CoolVibes,
 
 	// KewlSwap
-	KEWLSWAPCOM: KewlSwap,
-	KEWLSWAPIO:  KewlSwap,
-	KEWLSWAPAPP: KewlSwap,
+	KEWLSWAPCOM:    KewlSwap,
+	KEWLSWAPIO:     KewlSwap,
+	KEWLSWAPAPP:    KewlSwap,
+	APIKEWLSWAPCOM: KewlSwap,
+	APIKEWLSWAPIO:  KewlSwap,
+	APIKEWLSWAPAPP: KewlSwap,
 }
 
 func HostToDomain(domain DomainName) (DomainKind, bool) {
